b039515cc657b65d3e2f3fda9bea9ee872ec7754
EricKrg <eric.krueger@uni-jena.de>
2019-04-06 12:57:28 +0200

    added visualizing

---
+++ vis.py
@@ -0,0 +1,66 @@
+import csv
+import sys
+import os
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+inData = sys.argv[1]
+time, percip, sim,real = ([] for i in range(4))
+with open(inData) as csv_file:
+    reader = csv.reader(csv_file, delimiter='\t')
+    line = 0
+    for row in reader:
+        if line == 0:
+            line=+1
+        else:
+            print(row)
+            try:
+                time.append(row[0])
+                percip.append(row[1])
+                sim.append(row[2])
+                real.append(row[3])
+            except IndexError:
+                print("end")
+# create data traces
+# sim
+trace = go.Scatter(
+    x = time[0:-1],
+    y = sim,
+    line = dict(
+        color = ('rgb(200, 0, 0)'),
+        width = 1.5),
+    name = 'simulierter Abfluss'
+)
+
+# real
+trace2 = go.Scatter(
+    x = time[0:-1],
+    y = real,
+    line = dict(
+        width = 1.5),
+    name = 'gemessener Abfluss'
+)
+# percip
+trace3 = go.Scatter(
+    x = time[0:-1],
+    y = percip,
+    line = dict(
+        color = ('rgb(0, 0, 100)'),
+        width = 0.5,
+        dash = 'dot'),
+    name = 'Niederschlag'
+)
+
+
+# layout stuff
+layout = dict(title = '<b>Simulierter und gemessener Abfluss</b>'.format(os.path.basename(inData)),
+              xaxis = dict(title = '<b>Zeit</b>'),
+              yaxis = dict(title = '<b>Abfluss(mm/s) /Niederschlag(mm)</b>'),
+              )
+data = [trace3,trace,trace2]
+
+fig = dict(data=data, layout=layout)
+
+plot(fig, filename='final.html')
+
+
